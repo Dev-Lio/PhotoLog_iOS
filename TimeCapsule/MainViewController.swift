a0d1374b9dac0b3e8d03b8821f91f2313ccdd629
Lio <ywsung@mempic.net>
2022-11-02 00:33:08 +0900

    1. First Commit

--- TimeCapsule/MainViewController.swift
+++ TimeCapsule/MainViewController.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbContent: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lbTitle.text = "TimeCapsule"
+        lbContent.text = "Git Test"
+        
     }
 
 
