f35e0afee05aec5b2c62cda49890104270b9033c
Lio <ywsung@mempic.net>
2022-11-03 17:51:04 +0900

    Main TabbarController 추가

---
+++ TimeCapsule/View/MainTabbarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabbarController.swift
+//  TimeCapsule
+//
+//  Created by Lio on 2022/11/03.
+//
+import UIKit
+
+class MainTabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
+
