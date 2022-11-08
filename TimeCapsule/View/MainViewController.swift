9df3fea89214687bb591d40c57c8a32e6f7c6d2e
Lio <since20201005@gmail.com>
2022-11-08 18:22:01 +0900

    commit

--- TimeCapsule/View/MainViewController.swift
+++ TimeCapsule/View/MainViewController.swift
@@ -15,12 +15,9 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
         
-        lbTitle.text = "TimeCapsule"
-        lbContent.text = "Main Page"
         
     }
-
-
 }
 
