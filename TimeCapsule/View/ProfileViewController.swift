84321c573943bb6946eaac6cadb51de060cedd4a
Lio <since20201005@gmail.com>
2022-11-22 22:50:11 +0900

    commit

---
+++ TimeCapsule/View/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  TimeCapsule
+//
+//  Created by Lio on 2022/11/22.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
