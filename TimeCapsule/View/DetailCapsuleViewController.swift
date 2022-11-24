8ae9ca5fbce66990010cb09336c4de20a4cde233
Lio <since20201005@gmail.com>
2022-11-24 23:10:19 +0900

    commit

---
+++ TimeCapsule/View/DetailCapsuleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailCapsuleViewController.swift
+//  TimeCapsule
+//
+//  Created by Lio on 2022/11/24.
+//
+
+import Foundation
+import UIKit
+
+class DetailCapsuleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
